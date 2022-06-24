5b914991c1a4448817d14ca4056b29359cf49e35
kkarakow <66139022+kkarakow@users.noreply.github.com>
2022-06-24 11:13:01 -0400

    Delete SQLQuery_3.sql

--- SQLQuery_3.sql
+++
@@ -1,5 +0,0 @@
-SELECT b.department_id, department_name, COUNT(*) AS Emp_Count
-FROM employees a 
-    INNER JOIN DEPARTMENTS b ON (a.department_id = b.department_id)
-GROUP BY b.department_name, b.department_id  
-HAVING AVG(salary) > 5000;
