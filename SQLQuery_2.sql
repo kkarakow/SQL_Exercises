d8fa51f51bddc28295a57d36d8b16d08c84aad53
kkarakow <66139022+kkarakow@users.noreply.github.com>
2022-06-24 11:12:55 -0400

    Delete SQLQuery_2.sql

--- SQLQuery_2.sql
+++
@@ -1,5 +0,0 @@
-SELECT b.department_name, COUNT(*) AS Emp_Count
-FROM employees a 
-    INNER JOIN DEPARTMENTS b ON (a.department_name = b.department_name)
-GROUP BY b.department_name    
-HAVING count(AVG(salary)) > 5000;
