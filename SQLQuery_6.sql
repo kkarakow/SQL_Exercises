4d6911fe40e445a0e00596631648ae5aa1d34a43
kkarakow <66139022+kkarakow@users.noreply.github.com>
2022-06-24 11:13:26 -0400

    Delete SQLQuery_6.sql

--- SQLQuery_6.sql
+++
@@ -1,6 +0,0 @@
-SELECT a.manager_id, b.first_name, b.last_name
-FROM employees a 
-    INNER JOIN employees b ON (a.manager_id = b.employee_id)
-        WHERE a.manager_id = a.manager_id
-GROUP BY a.manager_id, b.first_name, b.last_name
-HAVING count(a.employee_id) > 2;            
